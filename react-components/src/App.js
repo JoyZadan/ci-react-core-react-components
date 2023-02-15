637146ca6040d0079fb9983707883dd12aadc068
JoyZadan <joyzadan.learning@gmail.com>
2023-02-15 14:48:23 +0000

    Docs: added class components notes and solutions

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,13 +1,14 @@
+import logo from './logo.svg'
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
+// import FunctionalGreeting from "./components/FunctionalGreeting";
 import StatefulGreeting from "./components/StatefulGreeting";
-import HooksCounter from "./components/HooksCounter";
+// import HooksCounter from "./components/HooksCounter";
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting name="Joy" />
-      {/* <StatefulGreeting name="Anna" /> */}
+      {/* <FunctionalGreeting name="Joy" /> */}
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Joy" />
       {/* <HooksCounter name="Mike" /> */}
     </div>
   );
--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -1,28 +1,38 @@
 import React from "react";
 
-export default class StatefulGreeting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+// export default class StatefulGreeting extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
 
-  setCount = () => {
-    this.setState(prevState => {
-      return { count: prevState.count + 1 }
-    })
-  }
+//   setCount = () => {
+//     this.setState(prevState => {
+//       return { count: prevState.count + 1 }
+//     })
+//   }
 
+//   render() {
+//     return (
+//       <div className="greeting">
+//         <h1>
+//           Hello, {this.props.name}, I'm a stateful component!
+//         </h1>
+//         <h2>You've clicked {this.state.count} times</h2>
+//         <button onClick={this.setCount}>Increment Count</button>
+//       </div>
+//     );
+//   }
+// }
+
+// To turn this into a React component, we must extend the built-in Component class
+// from React by adding extends React.Component in the class definition
+class StatefulGreeting extends React.Component {
   render() {
-    return (
-      <div className="greeting">
-        <h1>
-          Hello, {this.props.name}, I'm a stateful component!
-        </h1>
-        <h2>You've clicked {this.state.count} times</h2>
-        <button onClick={this.setCount}>Increment Count</button>
-      </div>
-    );
+      return <h1>Hello, {this.props.name}. {this.props.greeting}</h1>
   }
 }
+
+export default StatefulGreeting;
