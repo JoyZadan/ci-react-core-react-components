d362657641dfbfa01da5024a81d254653e8729a4
JoyZadan <joyzadan.learning@gmail.com>
2023-02-28 18:57:39 +0000

    Added cPascalCase naming convention omments to statefulgreeting.js

--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -28,6 +28,8 @@ import React from "react";
 // }
 
 // First, create a class component by using the 'class' keyword
+// Component names should always be PascalCase so React can tell them apart from regular
+// HTML elements when we include them in our JSX,
 // To turn this into a React component, we must extend the built-in Component class
 // from React by adding extends React.Component in the class definition
 class StatefulGreeting extends React.Component {
